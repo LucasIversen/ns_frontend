a688e92e09d2fa9bdfaa6d91f193acc05a87badc
Lucas Iversen <43264353+LucasIversen@users.noreply.github.com>
2024-12-03 22:12:53 +0100

    better phone selector

--- src/components/Roster/index.tsx
+++ src/components/Roster/index.tsx
@@ -8,6 +8,18 @@ import { db } from "../../firebase";
 const Roster = () => {
   const [activeTab, setActiveTab] = useState("offence");
   const [players, setPlayers] = useState<Player[]>([]);
+  const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth <= 600);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 600);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup the event listener when the component is unmounted
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   useEffect(() => {
     const fetchPlayers = async () => {
@@ -73,48 +85,93 @@ const Roster = () => {
 
   return (
     <div className="roster-page">
-      <div className="tabs">
-        <div className="tab-container">
-          <div
-            className="tab-indicator"
-            style={{
-              transform: `translateX(${
-                activeTab === "offence"
-                  ? 0
-                  : activeTab === "defence"
-                  ? 100
-                  : activeTab === "special"
-                  ? 200
-                  : 300
-              }%)`,
-            }}
-          ></div>
-          <button
-            onClick={() => handleTabChange("offence")}
-            className={activeTab === "offence" ? "active" : ""}
-          >
-            Offense
-          </button>
-          <button
-            onClick={() => handleTabChange("defence")}
-            className={activeTab === "defence" ? "active" : ""}
-          >
-            Defense
-          </button>
-          <button
-            onClick={() => handleTabChange("special")}
-            className={activeTab === "special" ? "active" : ""}
-          >
-            Special Teams
-          </button>
-          <button
-            onClick={() => handleTabChange("coaches")}
-            className={activeTab === "coaches" ? "active" : ""}
-          >
-            Coaches
-          </button>
+      {!isMobile ? (
+        <div className="tabs">
+          <div className="tab-container">
+            <div
+              className="tab-indicator"
+              style={{
+                transform: `translateX(${
+                  activeTab === "offence"
+                    ? 0
+                    : activeTab === "defence"
+                    ? 100
+                    : activeTab === "special"
+                    ? 200
+                    : 300
+                }%)`,
+              }}
+            ></div>
+            <button
+              onClick={() => handleTabChange("offence")}
+              className={activeTab === "offence" ? "active" : ""}
+            >
+              Offense
+            </button>
+            <button
+              onClick={() => handleTabChange("defence")}
+              className={activeTab === "defence" ? "active" : ""}
+            >
+              Defense
+            </button>
+            <button
+              onClick={() => handleTabChange("special")}
+              className={activeTab === "special" ? "active" : ""}
+            >
+              Special Teams
+            </button>
+            <button
+              onClick={() => handleTabChange("coaches")}
+              className={activeTab === "coaches" ? "active" : ""}
+            >
+              Coaches
+            </button>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="vertical-tab-selector">
+          <div className="vertical-tab-container">
+            <div
+              className="vertical-tab-indicator"
+              style={{
+                transform: `translateY(${
+                  activeTab === "offence"
+                    ? 0
+                    : activeTab === "defence"
+                    ? 100
+                    : activeTab === "special"
+                    ? 200
+                    : 300
+                }%)`,
+              }}
+            ></div>
+            <button
+              onClick={() => setActiveTab("offence")}
+              className={activeTab === "offence" ? "vertical-tab-active" : ""}
+            >
+              Offense
+            </button>
+            <button
+              onClick={() => setActiveTab("defence")}
+              className={activeTab === "defence" ? "vertical-tab-active" : ""}
+            >
+              Defense
+            </button>
+            <button
+              onClick={() => setActiveTab("special")}
+              className={activeTab === "special" ? "vertical-tab-active" : ""}
+            >
+              Special Teams
+            </button>
+            <button
+              onClick={() => setActiveTab("coaches")}
+              className={activeTab === "coaches" ? "vertical-tab-active" : ""}
+            >
+              Coaches
+            </button>
+          </div>
+        </div>
+      )}
 
       <div className="grid-container">
         <div className="grid">
